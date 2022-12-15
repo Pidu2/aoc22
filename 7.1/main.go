b00f0553022c8b74415df97b9db088ad092fa5eb
Beat Schärz <beatschaerz@hotmail.com>
2022-12-15 13:22:02 +0100

    7 chaining does not work yet

--- 7.1/main.go
+++ 7.1/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Node struct {
 	parent   *Node
-	children []*Node
+	children map[string]*Node
 	name     string
 	files    map[string]int
+}
+
+func (n Node) show() {
+	fmt.Println("Name ", n.name)
+	fmt.Printf("Children: %v\n", n.children)
+	fmt.Printf("Parent: %v\n", n.parent)
+	fmt.Printf("files: %v\n", n.files)
+	fmt.Println("#############")
 }
 
 func main() {
@@ -40,5 +51,30 @@ func main() {
 
 	for scanner.Scan() {
 		line := scanner.Text()
+		currentNode.show()
+		fmt.Println(line)
+		if line == "$ cd /" {
+			currentNode.name = "/"
+			currentNode.files = make(map[string]int)
+			currentNode.children = make(map[string]*Node)
+		} else if line == "$ cd .." {
+			currentNode = *currentNode.parent
+		} else if strings.HasPrefix(line, "$ cd") {
+			currentNode = *currentNode.children[strings.Split(line, " ")[2]]
+		} else if strings.HasPrefix(line, "$ ls") {
+			continue
+		} else if strings.HasPrefix(line, "dir") {
+			currentNode.children[strings.Split(line, " ")[1]] = &Node{
+				parent:   &currentNode,
+				name:     strings.Split(line, " ")[1],
+				files:    make(map[string]int),
+				children: make(map[string]*Node),
+			}
+		} else {
+			split := strings.Split(line, " ")
+			name := split[1]
+			size, _ := strconv.Atoi(split[0])
+			currentNode.files[name] = size
+		}
 	}
 }
